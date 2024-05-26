d88525777c4c05edda521acd665754b973bd714d
tonmoy-dev <rxdevs100@gmail.com>
2024-05-26 23:32:12 +0600

    Add email verify code api

---
+++ src/app/api/email-verify-code/route.ts
@@ -0,0 +1,50 @@
+import { dbConnect } from "@/lib/dbConnect";
+import UserModel from "@/model/user.model";
+import { createJSONResponse } from "@/utility/createResponse";
+
+// create POST request
+export async function POST(request: Request) {
+  // connected to DB
+  await dbConnect();
+
+  try {
+    // get username & verify code from request
+    const { username, code } = await request.json();
+
+    // decoded URI params
+    const decodedUsername = decodeURIComponent(username);
+    // get user from DB
+    const user = await UserModel.findOne({ username: decodedUsername });
+
+    if (!user) return createJSONResponse(false, "User not found", 404);
+
+    // check if the verify code is correct & not expired
+    const isCodeValid = user.verifyCode === code;
+    const isCodeNotExpired = new Date(user.verifyCodeExpiry) > new Date();
+
+    if (!isCodeValid)
+      return createJSONResponse(false, "Incorrect verify code", 400);
+    else if (!isCodeNotExpired)
+      return createJSONResponse(
+        false,
+        "Verify code has expired, please sign up again to get a new code",
+        400
+      );
+    else {
+      // isCodeValid & isCodeNotExpired is true
+      // update the user's verification status
+      user.isVerified = true;
+      // update the user to DB
+      await user.save();
+
+      return createJSONResponse(
+        true,
+        "User account verified successfully",
+        200
+      );
+    }
+  } catch (error) {
+    console.error("Getting errors while verifying user:", error);
+    return createJSONResponse(false, "Error verifying user", 500);
+  }
+}
